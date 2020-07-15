1ef5c2b6c32de080da7dd4b66b3752bcc945b009
Yoni Goriachnick <yonig770@gmail.com>
2020-07-15 18:40:03 +0300

    added some sample code in example app change pod spec description changed pod version

--- Example/LocaLite/ViewController.swift
+++ Example/LocaLite/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 import LocaLite
 
 class ViewController: UIViewController {
-
+    
+    
+    var languages = [String]()
+    var currentUserLanguage: String!
+    var currentSelection: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        LocaLite.printName()
+//        LocaLite.printName()
+        
+//        languages = LocaLite.getAvailableLanguages() ?? []
+//        currentUserLanguage = LocaLite.getUserSelectedLang()
+//        currentSelection = currentUserLanguage
+//
+//        LocaLite.setUserSelectedlangCodeLang(with: self.currentSelection)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,5 +33,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentSelection = languages[indexPath.row]
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return languages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "langTableViewCell", for: indexPath) as UITableViewCell
+        let langCode = languages[indexPath.row]
+//        guard let language = LocaLite.getLanguageDisplayName(langCode: langCode) else {
+//            return cell
+//        }
+        cell.textLabel?.text = langCode
+        cell.isSelected = langCode == currentSelection
+        return cell
+    }
 }
 
--- LocaLite.podspec
+++ LocaLite.podspec
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-A lite weight, easy-to-use Localization SDK for iOS apps written in Swift
+A lightweight, easy-to-use Localization SDK for iOS apps written in Swift
                        DESC
 
   s.homepage         = 'https://github.com/yoni-g/LocaLite'
@@ -28,7 +28,7 @@ A lite weight, easy-to-use Localization SDK for iOS apps written in Swift
   s.source           = { :git => 'https://github.com/yoni-g/LocaLite.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
 
   s.source_files = 'LocaLite/Classes/**/*'
   
