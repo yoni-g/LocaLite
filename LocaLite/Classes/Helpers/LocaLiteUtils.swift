fb9054ec863a7115bc0c2527075d0d02e179543d
Yonathan Goriachnick <yonig770@gmail.com>
2021-06-15 13:47:04 +0300

    added some util functions for languages display names

--- LocaLite/Classes/Helpers/LocaLiteUtils.swift
+++ LocaLite/Classes/Helpers/LocaLiteUtils.swift
@@ -13,4 +13,32 @@ final internal class LocaLiteUtils {
         let data = UserDefaults.standard
         return data.object(forKey: key.rawValue) as AnyObject?
     }
+    
+    func getShortLangCode(of languageCode: String) -> String{
+        return languageCode.contains("-") ? String(languageCode.split(separator: "-").first ?? "") : languageCode
+    }
+
+    func getLocalDisplayName(for languageCode: String) -> String{
+        // first take the english label
+        let langCode = getShortLangCode(of: languageCode)
+        // now let's take the language's own display name
+        let locale = NSLocale(localeIdentifier: langCode)
+        let currentLangLabel = locale.displayName(forKey: .identifier, value: langCode) ?? ""
+        return currentLangLabel
+    }
+    
+    func getEnglishDisplayName(for languageCode: String) -> String{
+        let langCode = getShortLangCode(of: languageCode)
+        // first take the english label
+        let enLocale = NSLocale(localeIdentifier: "en")
+        let englishLangName = enLocale.displayName(forKey: .identifier, value: langCode) ?? ""
+        return englishLangName
+    }
+    
+    func getDisplayNameWithLocalName(for languageCode: String, with: Bool) -> String{
+        let langCode = getShortLangCode(of: languageCode)
+        return """
+            \(getEnglishDisplayName(for: languageCode)) (\(getLocalDisplayName(for: langCode)))
+        """
+    }
 }
